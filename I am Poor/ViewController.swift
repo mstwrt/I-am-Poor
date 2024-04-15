c37e74d812de974e00d8e55b19ffcb552bad2220
Mstwrt <mstwrt@gmail.com>
2024-04-15 14:05:55 -0600

    Initial Commit

---
+++ I am Poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Poor
+//
+//  Created by Marcus on 2024-04-15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
